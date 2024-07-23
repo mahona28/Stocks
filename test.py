7b62b224304062a21ddd1ac678b1b4c7abc5dce2
Vemund Handeland <vemundhandeland@gmail.com>
2024-07-23 12:10:22 +0200

    made test.py show forwardPE from all stocks in stocks.txt

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import yfinance as yf
 #from pandas_datareader import data as pdr
-
-msft = yf.Ticker("2020.OL")
-print(msft.info)
+f = open("stocks.txt", "r")
+stocks = f.read()
+stocks = stocks.split("\n")
+for stock in stocks:
+    stock = stock + ".OL"
+    print(stock)
+    try:
+      msft = yf.Ticker(stock)
+      print(msft.info['forwardPE'])
+    except:
+      print("Error")
