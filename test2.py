a0c28a44b6f89d00db8af6dcadca911cf5b3e968
Vemund Handeland <vemundhandeland@gmail.com>
2024-07-23 16:02:48 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,15 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-
-driver = webdriver.Chrome()
-driver.get("https://live.euronext.com/nb/markets/oslo/equities/euronext/list")
-driver.implicitly_wait(10)
-time.sleep(5)
-button = driver.find_element(By.CSS_SELECTOR,'a[data-dt-idx="1"]')
-try:
-    button.click()
-except:
-    print("Error")
-time.sleep(5)
-driver.quit()
+import yfinance as yf
+stock = yf.Ticker("YAR.OL")
+print(stock.info['quickRatio'])
